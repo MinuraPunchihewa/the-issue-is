bb22188d88a5e2fd81d9ca24d15f9c53a59aa8a7
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-08-23 00:18:55 +0530

    added EMS configurations to jest.config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,22 @@
 module.exports = {
-  testEnvironment: 'node',
-};
-
-module.exports = {
+  testEnvironment: 'node --experimental-vm-modules node_modules/jest/bin/jest.js',
   testMatch: [
     '**/tests/**/*.test.mjs'
-  ]
+  ],
+  transform: {},
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '\\.pnp\\.[^\\/]+$'
+  ],
+  moduleFileExtensions: [
+    'js',
+    'mjs',
+    'json',
+    'node'
+  ],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  }
 };
